81d28622fa3d53ec63ccb9c456b92259fd58afed
harshpathakzz <harshpathak2017@gmail.com>
2023-09-09 23:36:56 +0530

    updated

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,7 +6,11 @@ import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Auth0Provider>
+    <Auth0Provider
+      domain={import.meta.env.VITE_DOMAIN}
+      clientId={import.meta.env.VITE_CLIENT_ID}
+      redirectUri={window.location.origin}
+    >
       <App />
     </Auth0Provider>
   </React.StrictMode>
