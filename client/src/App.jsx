2d09e8ca89b08c82fb55a4e697b22eccdf4e8920
harshpathakzz <harshpathak2017@gmail.com>
2023-09-09 23:48:21 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,29 @@ import { useAuth0 } from "@auth0/auth0-react";
 import "./App.css";
 
 function App() {
-  const { isAuthenticated, loginWithRedirect, logout, loginWithPopup } =
+  const { isAuthenticated, loginWithRedirect, logout, loginWithPopup, user } =
     useAuth0();
+
+  // Convert user object to a JSON string
+  const userJSON = JSON.stringify(user, null, 2);
+
+  // Log user JSON to the console
+  console.log(userJSON);
 
   return (
     <div className="App">
       <h1>Hello World</h1>
       {isAuthenticated ? (
-        <button onClick={() => logout()}>Logout</button>
+        <div>
+          <p>Welcome, {user.name}</p>
+          <p>Email: {user.email}</p>
+          <p>
+            Picture: <img src={user.picture} alt={user.name} />
+          </p>
+          <p>User JSON:</p>
+          <pre>{userJSON}</pre> {/* Display user JSON in a pre element */}
+          <button onClick={() => logout()}>Logout</button>
+        </div>
       ) : (
         <div>
           <button onClick={() => loginWithRedirect()}>Login</button>
